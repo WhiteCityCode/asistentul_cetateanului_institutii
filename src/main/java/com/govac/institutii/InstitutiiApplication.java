a7784a12d9f50b6d82f1c0f43896f2bd6889eea3
Adrian Rosian <adrian.rosian@gmail.com>
2016-11-10 16:26:19 +0200

    Securtity, jpa postgres, rest and migrations working

---
+++ src/main/java/com/govac/institutii/InstitutiiApplication.java
@@ -0,0 +1,11 @@
+package com.govac.institutii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstitutiiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(InstitutiiApplication.class, args);
+	}
+}
